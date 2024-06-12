4ee5258d79adca7d4dd8b98dc01827533982902b
ozkun-code <fahrulrozi12tkr4@gmail.com>
2024-06-12 16:00:15 +0700

    change detail transaction page

--- resources/views/livewire/transaksi/detail.blade.php
+++ resources/views/livewire/transaksi/detail.blade.php
@@ -5,19 +5,15 @@
             <h3 class="font-bold text-lg">Detail Transaksi</h3>
             <div class="py-4 space-y-4">
                 <div class="flex flex-col">
-                    <div class="text-sm opacity-50">Tangggal Transaksi</div>
+                    <div class="text-sm opacity-50">Tanggal Transaksi</div>
                     <div>{{ $transaksi?->created_at->format('d F Y H:i') }}</div>
                 </div>
                 <div class="flex flex-col">
                     <div class="text-sm opacity-50">Nama Customer</div>
                     <div>{{ $transaksi?->customer?->name ?? '-' }}</div>
                 </div>
-                <div class="flex flex-col">
-                    <div class="text-sm opacity-50">Total Bayar</div>
-                    <div>Rp{{ Number::format($transaksi?->price ?? 0) }}</div>
-                </div>
             </div>
-            <div class="table-wraper">
+            <div class="table-wrapper">
                 <table class="table">
                     <thead>
                         <th>Nama Service</th>
@@ -25,8 +21,17 @@
                         <th>Harga</th>
                     </thead>
                     <tbody>
+                        @php
+                            $totalPembelian = 0;
+                            foreach (json_decode($transaksi?->items, true) ?? [] as $item) {
+                                $totalPembelian += $item['price'];
+                            }
+                            $discountNominal = ($totalPembelian * ($transaksi?->discount ?? 0)) / 100;
+                            $totalBayar = $totalPembelian - $discountNominal;
+                        @endphp
+
                         @foreach (json_decode($transaksi?->items, true) ?? [] as $key => $item)
-                            <tr>
+                            <tr class="table-row-bordered">
                                 <td>{{ $key }}</td>
                                 <td>{{ $item['qty'] }} @if (isset($item['unit']))
                                         {{ $item['unit'] }}
@@ -35,6 +40,21 @@
                                 <td>{{ Number::format($item['price']) }}</td>
                             </tr>
                         @endforeach
+                        <tr class="table-row-bordered-thick">
+                            <td>Total Pembelian</td>
+                            <td>-</td>
+                            <td>{{ Number::format($totalPembelian) }}</td>
+                        </tr>
+                        <tr class="table-row-bordered">
+                            <td>Discount</td>
+                            <td>{{ Number::format($transaksi?->discount ?? 0) }}%</td>
+                            <td>-{{ Number::format($discountNominal) }}</td>
+                        </tr>
+                        <tr class="table-row-bordered">
+                            <td>Total Bayar</td>
+                            <td>-</td>
+                            <td>{{ Number::format($totalBayar) }}</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
