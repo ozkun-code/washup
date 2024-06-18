75e6f837e3ee4303c9c7ee0d802a260ade54288b
ozkun-code <fahrulrozi12tkr4@gmail.com>
2024-06-18 18:34:40 +0700

    mail ip

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Rappasoft\LaravelAuthenticationLog\Traits\AuthenticationLoggable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,AuthenticationLoggable;
 
     /**
      * The attributes that are mass assignable.
